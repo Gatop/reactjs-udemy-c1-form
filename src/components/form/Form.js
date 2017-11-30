9f9bcc0abec3dd5be25a0707e4b7b1322e83852c
Jega <gatomzl@gmail.com>
2017-11-29 23:45:11 -0500

    React desde 0 - Formulario - Implementando Refs

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,31 +4,57 @@ import React, { Component } from 'react';
 import FormRow from './FormRow.js'
 
 class Form extends Component {
+
+    // Constructor del componente
+    constructor(){
+        super();
+
+        this.onRegister = this.onRegister.bind(this);
+    }
+
+    // Metodo que maneja el evento del boton
+    onRegister(e) {
+        console.log(this.refs.nombre.getValue());
+    }
+
+    // Metodo que maneja el submit del form
+    onSubmit(e){
+        e.preventDefault();
+    }
+
     // Metodo render
     render(){
         return(
             <div>
                 <h2>Registro</h2>
-                <FormRow
-                    inputType="Text"
-                    labelText="Nombre"
-                />
-                <FormRow
-                    inputType="Text"
-                    labelText="Apellidos"
-                />
-                <FormRow
-                    inputType="email"
-                    labelText="Correo Electronico"
-                />
-                <FormRow
-                    inputType="password"
-                    labelText="Password"
-                />
-                <FormRow
-                    inputType="password"
-                    labelText="Confirmar Password"
-                />
+                <form onSubmit={this.onSubmit}>
+                    <FormRow
+                        inputType="Text"
+                        labelText="Nombre"
+                        ref="nombre"
+                    />
+                    <FormRow
+                        inputType="Text"
+                        labelText="Apellidos"
+                        ref="apellidos"
+                    />
+                    <FormRow
+                        inputType="email"
+                        labelText="Correo Electronico"
+                        ref="email"
+                    />
+                    <FormRow
+                        inputType="password"
+                        labelText="Password"
+                        ref="password"
+                    />
+                    <FormRow
+                        inputType="password"
+                        labelText="Confirmar Password"
+                        ref="confirm-password"
+                    />
+                    <button onClick={this.onRegister}>Registro</button>
+                </form>
             </div>
         );
     }
